4d3faab83bc00ae49d719dfc5d1ec1327162937a
Victor Eckschmidt <Victor.eckschmidt@outlook.com.br>
2021-10-25 16:20:35 -0300

    Aula03

--- src/EstruturaSwitchAleatorioRename.java
+++ src/EstruturaSwitchAleatorioRename.java
@@ -1,25 +1,26 @@
-import java.util.Scanner;
+import java.util.Random;
 
-public class EstruturaSwitch {
+public class EstruturaSwitchAleatorioRename {
 
 	public static void main(String[] args) {
 		/*Crie um programa que permita ao usuário escolher o elogio que gostaria de receber, de acordo com a seguinte regra: 1 - Elogio profissional, 2 - Elogio físico, 3 - Elogio pessoal
 		 * Caso digite qualquer outro número, deve ser exibida a mensagem "opção inválida"*/
-		Scanner leitor = new Scanner(System.in);
 		int opcao;
 		System.out.println("ELOGIADOR!");
-		System.out.println("Selecione a opção desejada:\n1-Elogio profissional\n2-Elogio físico\n3-Elogio pessoal");
-		opcao = leitor.nextInt();
 	
 		//Aqui precisamos de uma estrutura para validar a opção inserida.
+		Random gerador = new Random();
+		opcao = gerador.nextInt(3);
+		System.out.println(opcao);
+		
 		switch (opcao) {
-		case 1:
+		case 0:
 			System.out.println("Os seus ifs são lindos demais!");
 			break;
-		case 2:
+		case 1:
 			System.out.println("A sua densidade óssea é incrivel!");
 			break;
-		case 3:
+		case 2:
 			System.out.println("Você é uma pessoa sensacional!");
 			break;
 		default:
@@ -28,8 +29,9 @@ public class EstruturaSwitch {
 	
 		}
 		
-		leitor.close();
+		//leitor.close();
 
 	}
 
 }
+
